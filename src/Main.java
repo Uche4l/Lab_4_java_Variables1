7ffb063f50f1cafc94b77b329f21ae239ceed83d
samue <mbasu@mail.uc.edu>
2024-09-19 15:08:29 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int int0operandA = 24;
+        int int0operandB = 14;
+
+        int intSum = 0;
+        int intProduct = 0;
+        int intQuotient = 0;
+        int intDifference = 0;
+        int intModulo = 0;
+
+        intSum = int0operandA + int0operandB;
+        System.out.println("The sum of " + int0operandA + " and " + int0operandB + " is " + intSum);
+        intProduct = int0operandA * int0operandB;
+        System.out.println();
+
+
+        // complete this section
+
+
+        double double0perandA = 24.24
+        double double0perandB = 14.14
+
+        double doubleSum = 0;
+        double doubleProduct = 0;
+        double doubleQuotient = 0;
+        double doubleDifference = 0;
+
+        doubleSum = double0perandA + double0perandB;
+
+        // complete this for doubles
+
+
+
+
+
+    }
+}
