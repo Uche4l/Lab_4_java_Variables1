222c9c995bca090ab3e13bd73163d94ec0e1fb00
samue <mbasu@mail.uc.edu>
2024-09-20 11:29:00 -0400

    init commit

--- src/Main.java
+++ src/Main.java
@@ -14,14 +14,20 @@ public class Main
         intSum = int0operandA + int0operandB;
         System.out.println("The sum of " + int0operandA + " and " + int0operandB + " is " + intSum);
         intProduct = int0operandA * int0operandB;
-        System.out.println();
+        System.out.println("the product of" + int0operandA + " and " + int0operandB + " is " + intProduct);
+        intQuotient = int0operandA / int0operandB;
+        System.out.println("the quotient of" + int0operandA + " and " + int0operandB + " is " + intQuotient);
+        intDifference = int0operandA - int0operandB;
+        System.out.println("the difference of " + int0operandA + " and " + int0operandB + " is " + intDifference);
+        intModulo = int0operandA % int0operandB;
+        System.out.println("the modulo" + int0operandA + " and " + int0operandB + " is " + intModulo);
 
 
         // complete this section
 
 
-        double double0perandA = 24.24
-        double double0perandB = 14.14
+        double double0perandA = 24.24;
+        double double0perandB = 14.14;
 
         double doubleSum = 0;
         double doubleProduct = 0;
@@ -29,12 +35,13 @@ public class Main
         double doubleDifference = 0;
 
         doubleSum = double0perandA + double0perandB;
-
-        // complete this for doubles
-
-
-
-
+        System.out.println("the sum of "+ double0perandA + " and " + double0perandB + " is " + doubleSum);
+        doubleProduct = double0perandA * double0perandB;
+        System.out.println("the product of " + double0perandA + " and " + double0perandB + " is " + doubleProduct);
+        doubleQuotient = double0perandA / double0perandB;
+        System.out.println("the qoutient " + double0perandA + " and " + double0perandB + " is " + doubleQuotient);
+        doubleDifference = double0perandA - double0perandB;
+        System.out.println("the difference of " + double0perandA + " and " + double0perandB + " is " + doubleDifference);
 
     }
 }
